30020e651661702bda8ade475deb673f17bcf268
JuliMonte5 <julian.montenegro@radiumrocket.com>
2025-10-21 16:34:04 -0300

    add dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 // import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+// import App from "./App.tsx";
+import { FormDemo } from "./pages/FormDemo/FormDemo.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<FormDemo />);
